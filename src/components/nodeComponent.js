b4e440a69c797b982db8540e785dd18ccf053440
Mahavishnu <mahavishnureddya@gmail.com>
2021-02-16 22:20:27 -0500

    update

---
+++ src/components/nodeComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import axios from "axios";
+ const NodeComp=()=>{
+   axios.get("http://localhost:5000"+code)
+  return (
+  <asdasdaa>
+      <h1>this is Comp</h1>
+      </asdasdaa>
+  );
+}
+
+export default NodeComp;
