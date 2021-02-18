9726e22c7c4c5a4803e6bb193c9e413b5d6c7ff4
Mahavishnu <mahavishnureddya@gmail.com>
2021-02-18 14:54:36 -0500

    InputCompAdded

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
 import NodeComp from './components/nodeComponent';
+import ErrorInput from './components/ErrorInput';
 function App() {
   return (
     <div>
    <h1>Hi</h1>
+   <ErrorInput/>
     <NodeComp />
     </div>
   );
--- src/components/nodeComponent.js
+++ src/components/nodeComponent.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import axios from "axios";
  const NodeComp=()=>{
-   axios.get("http://localhost:5000"+code)
+   
   return (
   <asdasdaa>
       <h1>this is Comp</h1>
